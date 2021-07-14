f3a78d2d7c3897ba475e7b0748538d8d43e4c1c8
hanjae do <dohanjae@gmail.com>
2021-07-14 09:08:22 +0900

    백준 1단계

---
+++ baek/src/baek_1stage/part2.java
@@ -0,0 +1,8 @@
+package baek_1stage;
+
+public class part2 {
+	public static void main(String[] args) {
+		System.out.println("강한친구 대한 육군");
+		System.out.println("강한친구 대한 육군");
+	}
+}
---
+++ baek/src/baek_1stage/part3.java
@@ -0,0 +1,10 @@
+package baek_1stage;
+
+public class part3 {
+	public static void main(String[] args) {
+		System.out.println("\\    /\\");
+		System.out.println(" )  ( ')");
+		System.out.println("(  /  )");
+		System.out.println(" \\(__)|");
+	}
+}
