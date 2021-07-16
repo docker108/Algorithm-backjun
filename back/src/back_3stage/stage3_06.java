21c6fdb9fb49cb56f452105567fc99cf96bcd079
hanjae do <dohanjae@gmail.com>
2021-07-16 14:55:18 +0900

    3-7

--- back/src/back_3stage/stage3_06.java
+++ back/src/back_3stage/stage3_06.java
@@ -2,16 +2,15 @@ package back_3stage;
 
 import java.util.Scanner;
 
-public class stage3_05 {
+public class stage3_06 {
 	
 	public static void main(String[] args) {
 		
-		// 순서대로 숫자 찍기.
 		Scanner sc = new Scanner(System.in);
 		
 		int num = sc.nextInt();
 		
-		for (int i = 1; i <= num; i++) {
+		for (int i = num; i > 0; i--) {
 			System.out.println(i);
 		}
 		
