d4ede383e0b416ab7a036f87c1b11bd0bb7f5499
hanjae do <dohanjae@gmail.com>
2021-07-16 09:54:10 +0900

    백준 3-5

--- back/src/back_3stage/stage3_05.java
+++ back/src/back_3stage/stage3_05.java
@@ -6,6 +6,7 @@ public class stage3_05 {
 	
 	public static void main(String[] args) {
 		
+		// 순서대로 숫자 찍기
 		Scanner sc = new Scanner(System.in);
 		
 		int num = sc.nextInt();
--- baek/src/back_1stage/part1.java
+++
@@ -1,7 +0,0 @@
-package back_1stage;
-
-public class part1 {
-	public static void main(String[] args) {
-		System.out.println("Hello World!");
-	}
-}
