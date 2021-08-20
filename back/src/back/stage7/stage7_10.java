2fc342d758f8bef0c94447e1e81b1c35fca6e60a
hanjae do <godohanjae@gmail.com>
2021-08-21 00:25:03 +0900

    7-10 ~ing

--- back/src/back/stage7/stage7_10.java
+++ back/src/back/stage7/stage7_10.java
@@ -5,22 +5,22 @@ import java.util.Scanner;
 public class stage7_10 {
 	public static void main(String[] args) {
 		
-//		Scanner sc = new Scanner(System.in);
-//		
-//		int start = sc.nextInt();
-//		
-//		int count = 0;
-//		
-//		for (int i = 0; i < start; i++) {
-//			
-//			String value = sc.next();
-//			
-//			String[] arr = value.split("");
-//			
-//			for (int j = 0; j < arr.length; j++) {
-//				String compare = arr[j];
-//				
-//			}
-//		}
+		Scanner sc = new Scanner(System.in);
+		
+		int start = sc.nextInt();
+		
+		int count = 0;
+		
+		for (int i = 0; i < start; i++) {
+			
+			String value = sc.next();
+			
+			String[] arr = value.split("");
+			
+			for (int j = 0; j < arr.length; j++) {
+				String compare = arr[j];
+				
+			}
+		}
 	}
 }
